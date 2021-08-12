fd9bf9c48b96a399ac49a71c2148595918694a1d
Thomas McFarlane <t.h.mcfarlane1@googlemail.com>
2021-08-12 17:07:08 +0100

    Exponential model working. Log-logistic needs to be fixed

--- likelihood functions.R
+++ likelihood functions.R
@@ -1,3 +1,15 @@
+### Exponential ###
+nll <- function(theta, data){
+  obs <- which(data$d == 1)
+  
+  lambda <- 1 / exp(theta[1] + theta[2] * data$tmt)
+  ll <- sum(dexp(data$t[obs], lambda[obs], log=TRUE))
+  ll <- ll + sum(pexp(data$t[-obs], lambda[-obs], lower.tail = FALSE, log.p = TRUE))
+  
+  return(-ll)
+}
+
+### Weibull ###
 # Weibull density function using alternate parameterisation
 dweib <- function(t, shape, scale, log = FALSE){
   rescale <- (1 / scale) ^ (1 / shape)
@@ -17,6 +29,47 @@ nll <- function(theta, data){
   
   ll <- sum(dweib(data$t[obs], p, lambda[obs], log = TRUE))
   ll <- ll + sum(pweib(data$t[-obs], p, lambda[-obs], lower.tail = FALSE, log.p = TRUE))
+  return(-ll)
+}
+
+### Log-logistic ###
+dloglog <- function(x, shape, scale, log = FALSE){
+  density <- (scale * shape * x ** (shape - 1)) / ((1 + scale * x ** shape) ** 2)
+  
+  if(log){
+    log(density)
+  }
+  else{
+    density
+  }
+}
+
+ploglog <- function(q, shape, scale, lower.tail = TRUE, log.p = FALSE){
+  prob <- 1 / (1 + (scale * q ** shape) ** -1)
+  
+  if(!lower.tail){
+    prob <- 1 - prob
+  }
+  else{
+  }
+  
+  if(log.p){
+    log(prob)
+  }
+  else{
+    prob
+  }
+}
+
+nll <- function(theta, data){
+  obs <- which(data$d == 1)
+  
+  shape <- exp(theta[1])
+  scale <- exp(theta[2] + theta[3] * data$tmt) ^ (-shape)
+  
+  ll <- sum(dloglog(data$t[obs], shape, scale[obs], log = TRUE))
+  ll <- ll + sum(ploglog(data$t[-obs], shape, scale[-obs], lower.tail = FALSE, log.p = TRUE))
+  
   return(-ll)
 }
 
