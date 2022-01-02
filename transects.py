60a416f24878b5eebda5c97a6e93b9f299162386
= <t.h.mcfarlane1@googlemail.com>
2022-01-02 22:57:08 +0000

    Corrected transect spline vertices

--- transects.py
+++ transects.py
@@ -1,21 +1,17 @@
 file = open("transects.txt", "w")
 file.write("# line X Y\n")
+file.write("1 -100 -100\n")
+X = -100
 
 for i in range(0, 11):
     Y = -100 + 20 * i
+    X = -X
     
-    if i % 2 == 0:
-        X = -100    
-    elif i % 2 != 0:
-        X = 100
-        
     line = "1 {0} {1}\n".format(X, Y)
     file.write(line)
-    line = "1 {0} {1}\n".format(-X, Y)
-    file.write(line)
-
+    
     if i != 10:
-        line = "1 {0} {1}\n".format(-X, Y + 20)
+        line = "1 {0} {1}\n".format(X, Y + 20)
         file.write(line)
 
 file.close()
