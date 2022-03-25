e4ba978870cfa177565b9c230d85467b73bd82c0
= <t.h.mcfarlane1@googlemail.com>
2022-03-25 20:49:54 +0000

    Transect file and code to generate it - has multiple transects now

--- transects.py
+++ transects.py
@@ -1,17 +1,14 @@
 file = open("transects.txt", "w")
 file.write("# line X Y\n")
-file.write("1 -100 -100\n")
-X = -100
+X = 100
 
-for i in range(0, 11):
-    Y = -100 + 20 * i
+for i in range(1, 12):
+    Y = -120 + 20 * i
     X = -X
     
-    line = "1 {0} {1}\n".format(X, Y)
+    line = "{0} {1} {2}\n".format(i, X, Y)
+    file.write(line)
+    line = "{0} {1} {2}\n".format(i, -X, Y)
     file.write(line)
     
-    if i != 10:
-        line = "1 {0} {1}\n".format(X, Y + 20)
-        file.write(line)
-
 file.close()
