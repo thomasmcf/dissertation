8c0237bd2701202b97bd82c69d75c828daf79417
= <t.h.mcfarlane1@googlemail.com>
2022-03-25 20:50:36 +0000

    Working simulation code

--- simulations.R
+++ simulations.R
@@ -1,8 +1,9 @@
+setwd("C:/Users/thmcf/Documents/Programming/Maths/Dissertation/dissertation")
 source("simulation functions.R")
 
 # Simulate the study site for three years, with N expected individuals
 N_exp <- 30
-pop <- population(N_exp, 365 * 3)
+pop <- population(N_exp, 365 * 101)
 
 # Create a blank plot - this will eventually show the spatial locations of activity centres and droppings
 plot(c(-SITE_LENGTH/2, SITE_LENGTH/2, -SITE_LENGTH/2, SITE_LENGTH/2),
@@ -10,6 +11,7 @@ plot(c(-SITE_LENGTH/2, SITE_LENGTH/2, -SITE_LENGTH/2, SITE_LENGTH/2),
      type = 'n', xlab = "Longitude", ylab = "Latitude")
 
 # Iterate over the population
+set.seed(12345)
 N <- length(pop)
 for(i in 1:N){
   ind <- pop[[i]]
@@ -37,12 +39,27 @@ legend(-100, 100, c("Present", "Departed", "Dropping"),
 B <- 1
 N_present <- numeric(B)
 N_detectable <- numeric(B)
+N_estimated <- numeric(B)
+set.seed(196)
+
+transect_list <- read.traps(file = "transects.txt", detector = "transect")
+x <- transect_list$x
+y <- transect_list$y
+
+x0 <- x[seq(1, 22, 2)]
+y0 <- y[seq(1, 22, 2)]
+x1 <- x[seq(2, 22, 2)]
+y1 <- y[seq(2, 22, 2)]
+
+pars <- list(lambda0 = 1, sigma = 30)
+
+mask <- make.mask(traps = transect_list, buffer = 0, spacing = 10)
 
 # Sys.time() # Time how long the simulations take
 # Run simulations - Takes ~7 minutes on my machine
 for(i in 1:B){
   # Pick a number of expected individuals
-  N_exp <- sample(5:200, 1)
+  N_exp <- 30
   # Generate 101 years of data
   # The survey is carried out at t=100 years
   # The first 100 years are for burn in, and to ensure no droppings would be present at t=100 that were dropped before t=0
@@ -61,34 +78,52 @@ for(i in 1:B){
       N_detectable[i] <- N_detectable[i] + 1
     }
   }
+  
+  print(i)
 }
 
+capthist <- survey_sim(x0, y0, x1, y1, pop, times = c(36500, 36530, 36560), HHN, pars)
+mod <- secr.fit(capthist = capthist, buffer = buffer)
+
 # Plot the results
-plot(N_present, N_detectable)
+#plot(N_present, N_detectable)
+
+plot <- ggplot() + geom_point(aes(x = N_present, y = N_detectable)) +
+  geom_abline(slope = 1, intercept = 0) +
+  xlab("Leopards Present") +
+  ylab("Leopards Detectable") +
+  theme_bw() +
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = 'black'))
+
 abline(0, 1)
 beep()
 
+
+
+N_present <- numeric(20)
+N_estimated <- numeric(20)
 # Sys.time() # End timing
-
-lambda <- function(d, lambda0, sigma){
-  lambda0 * exp(-d**2 / (2 * sigma**2))
+for(N_exp in seq(10, 200, 10)){
+  print(paste("Simulation", N_exp / 10))
+  print("    Simulating population")
+  pop <- population(N_exp, 365 * 101)
+  
+  print("    Simulating survey")
+  capthist <- survey_sim(x, y, pop, c(36500, 36530, 36560), HHN, pars)
+  print("    Fitting model")
+  mod <- secr.fit(capthist = capthist$secr_capthist, trace=F, mask = mask)
+  
+  N_estimated[N_exp / 10] <-  region.N(mod)[2, 1]
+  
+  for(j in 1:length(pop)){
+    # If animal j was present at t=100 years, increment the value in the presence vector
+    if(between(365 * 100, pop[[j]]$present[1], pop[[j]]$present[2])){
+      N_present[N_exp / 10] <- N_present[N_exp /10] + 1
+    }
+  }
 }
-
-HHN <- function(d, pars){
-  1 - exp(-lambda(d, pars$lambda0, pars$sigma))
-}
-
-transect_list <- read.traps(file = "transects.txt", detector = "transect")
-
-pop <- population(30, 365 * 101)
-x <- transect_list$x
-y <- transect_list$y
-pars <- list(lambda0 = 0.5, sigma = 50)
-
-capthist <- survey_sim(x, y, pop, seq(36500, 36500 + 12 * 30, 30))
-
-mask <- make.mask(traps = transect_list, buffer = 0, spacing = 10)
-
-# Fit model (or at least try)
-mod <- secr.fit(capthist = capthist$secr_capthist, trace=T, mask = mask)
-region.N(mod)
