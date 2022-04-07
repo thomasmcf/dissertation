588cfd4a1451c5bba504b4c4dc5bb78fa304df51
Thomas McFarlane <t.h.mcfarlane1@googlemail.com>
2022-04-07 15:56:18 +0100

    Minor changes

--- simulations.R
+++ simulations.R
@@ -3,8 +3,7 @@ source("simulation functions.R")
 sims <- read.csv("simulated.csv")
 
 # Simulate the study site for three years, with N expected individuals
-N_exp <- 30
-pop <- population(N_exp, 365 * 101)
+pop <- population(30, 365 * 101)
 
 # Create a blank plot - this will eventually show the spatial locations of activity centres and droppings
 plot(c(-SITE_LENGTH/2, SITE_LENGTH/2, -SITE_LENGTH/2, SITE_LENGTH/2),
@@ -20,7 +19,7 @@ for(i in 1:N){
   locations <- matrix(ind$location[, which(ind$drop_times <= 100 * 365 & ind$decay_times >= 100 * 365)], nrow = 2)
   # Plot these locations
   points(locations[1,], locations[2,], pch = 19, cex = 0.5, col = 'blue')
-  
+
   # Plot the activity centres, green if it's present at the start of the study, red if it is not
   if(ind$present[1] <= 100 * 365 & ind$present[2] >= 100 * 365){
     points(ind$activity_centre[1], ind$activity_centre[2], pch = 19, cex = 1.5, col = 'green')
@@ -34,14 +33,6 @@ for(i in 1:N){
 legend(-100, 100, c("Present", "Departed", "Dropping"),
        col = c('green', 'red', 'blue'),
        pch = 19)
- 
-# Now explore the issue of "prolonged detectability"
-# Define a number of simulations to run, and define vectors to store results
-B <- 1
-N_present <- numeric(B)
-N_detectable <- numeric(B)
-N_estimated <- numeric(B)
-set.seed(196)
 
 transect_list <- read.traps(file = "transects.txt", detector = "transect")
 x <- transect_list$x
@@ -54,75 +45,131 @@ y1 <- y[seq(2, 22, 2)]
 
 pars <- list(lambda0 = 1, sigma = 1)
 mask <- make.mask(traps = transect_list, buffer = 0, spacing = 10)
-
-# Sys.time() # Time how long the simulations take
-# Run simulations - Takes ~7 minutes on my machine
-for(i in 1:B){
-  # Pick a number of expected individuals
-  N_exp <- 30
-  # Generate 101 years of data
-  # The survey is carried out at t=100 years
-  # The first 100 years are for burn in, and to ensure no droppings would be present at t=100 that were dropped before t=0
-  # The last year is to avoid the tail behaviour that occurs as the simulation ends
-  pop <- population(N_exp, 365 * 101)
-  
-  Ns <- pres_det(pop, 365 * 100)
-  N_present[i] <- Ns$present
-  N_detectable[i] <- Ns$detectable
-    
-  print(i)
-}
-
 times <- c(36500, 36530, 36560)
-capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-mod <- secr.fit(capthist = capthist$secr_capthist, mask = mask)
-
-# Plot the results
-#plot(N_present, N_detectable)
-
-plot <- ggplot() + geom_point(aes(x = N_present, y = N_detectable)) +
-  geom_abline(slope = 1, intercept = 0) +
-  xlab("Leopards Present") +
-  ylab("Leopards Detectable") +
-  theme_bw() +
-  theme(axis.text=element_text(size=15),
-        axis.title=element_text(size=15),
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(colour = 'black'))
-
-abline(0, 1)
-beep()
 
 
+cl <- makeCluster(2)
+registerDoParallel(cl)
+getDoParWorkers()
 
-N_exp <- seq(30, 100, 2)
-N_pres <- numeric(length(N_exp))
-N_est <- numeric(length(N_exp))
-N_det <- numeric(length(N_exp))
-times <- c(36500, 36530, 36560)
-means <- numeric(length(N_exp))
 
-slope_data <- function(mean_life)
+# Simulate populations of different abundances - with surveys
+N_expected <- 1:200
+B <- length(N_expected)
 
-for(i in 1:length(N_est)){
-  print(paste("Simulation", i))
-  print("    Simulating population")
-  pop <- population(N_exp[i], 365 * 101)
-  pop <- prune(pop, 36500-30)
+output <- foreach(i = 1:B, .packages = c("tidyverse", "secr")) %dopar% {
+  N_exp <- N_expected[i]
+  pop <- prune(population(N_exp, 365 * 101), 36500 - 30)
   
-  print("    Simulating survey")
   capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-  print("    Fitting model")
-  mod <- secr.fit(capthist = capthist$secr_capthist, trace=F, mask = mask)
+  mod <- secr::secr.fit(capthist = capthist$secr_capthist, mask = mask)
   
-  N_est[i] <-  region.N(mod)[2, 1]
-  N_pres[i] <- pres_det(pop, 36500)$pres
-  N_det[i] <- pres_det(pop, 36500)$det
-  
-  print("    Survival Analysing")
-  sv <- make_surv_object(capthist$survhist, 36500-30, times)
+  list(expected = N_exp,
+       present = pres_det(pop, 36500)$pres,
+       detectable = pres_det(pop, 36500)$det,
+       estimated = region.N(mod)[2, 1]
+  )
 }
 
+df <- data.frame(expected = sapply(output, function(item) item$expected),
+                 present = sapply(output, function(item) item$present),
+                 detectable = sapply(output, function(item) item$detectable),
+                 estimated = sapply(output, function(item) item$esimated))
 
+saveRDS(df, "exp_pres_det_est.rds")
+
+
+
+# Simulate populations with different mean dropping life time
+mean_lifes <- seq(1, 365, 365)
+B <- length(mean_life)
+
+output <- foreach(i = 1:B, .packages = "tidyverse") %dopar% {
+  mean_life <- mean_lifes[i]
+  pop <- population(N_exp, 365 * 101, mean_life = mean_life)
+  
+  detectable_times <- sapply(pop,
+                                  function(ind) max(ind$degrade_times) - min(ind$drop_times))
+  
+  mean_detectable_time <- mean(detectable_times)
+  se <- sd(detectable_times) / sqrt(length(detectable_times))
+  
+  slope <- mean_detectable_time / (MEAN_STAY + (1 / DROP_RATE))
+  
+  list(mean_life = mean_life,
+       mean_detectable_time = mean_detectable_time,
+       se = se,
+       slope = slope
+  )
+}
+
+df <- data.frame(mean_life = sapply(output, function(item) item$mean_life),
+                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
+                 se = sapply(output, function(item) item$se),
+                 slope = sapply(output, function(item) item$slope))
+
+saveRDS(df, "dropping_life.rds")
+
+
+
+# Simulate populations with different dropping rates
+dropping_rate <- seq(1/7, 2, 50)
+B <- length(dropping_rate)
+
+output <- foreach(i = 1:B, .packages = "tidyverse") %dopar% {
+  drop_rate <- dropping_rate[i]
+  pop <- population(N_exp, 365 * 101, drop_rate = drop_rate)
+  
+  detectable_times <- sapply(pop,
+                             function(ind) max(ind$degrade_times) - min(ind$drop_times))
+  
+  mean_detectable_time <- mean(detectable_times)
+  se <- sd(detectable_times) / sqrt(length(detectable_times))
+  
+  slope <- mean_detectable_time / (MEAN_STAY + (1 / drop_rate))
+  
+  list(drop_rate = drop_rate,
+       mean_detectable_time = mean_detectable_time,
+       se = se,
+       slope = slope
+  )
+}
+
+df <- data.frame(drop_rate = sapply(output, function(item) item$drop_rate),
+                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
+                 se = sapply(output, function(item) item$se),
+                 slope = sapply(output, function(item) item$slope))
+
+saveRDS(df, "drop_rate.rds")
+
+
+
+# Simulate populations with different turnover rates
+mean_stays <- seq(30, 365 * 2, 50)
+B <- length(mean_stays)
+
+output <- foreach(i = 1:B, .packages = "tidyverse") %dopar% {
+  mean_stay <- mean_stays[i]
+  pop <- population(N_exp, 365 * 101, mean_stay = mean_stay)
+  
+  detectable_times <- sapply(pop,
+                             function(ind) max(ind$degrade_times) - min(ind$drop_times))
+  
+  mean_detectable_time <- mean(detectable_times)
+  se <- sd(detectable_times) / sqrt(length(detectable_times))
+  
+  slope <- mean_detectable_time / (mean_stay + (1 / DROP_RATE))
+  
+  list(mean_stay = mean_stay,
+       mean_detectable_time = mean_detectable_time,
+       se = se,
+       slope = slope
+  )
+}
+
+df <- data.frame(mean_stay = sapply(output, function(item) item$mean_stay),
+                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
+                 se = sapply(output, function(item) item$se),
+                 slope = sapply(output, function(item) item$slope))
+
+saveRDS(df, "drop_rate.rds")
