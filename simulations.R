e8b8957bfa757d932b474dde868720464938e432
Thomas McFarlane <t.h.mcfarlane1@googlemail.com>
2022-04-09 16:17:54 +0100

    foreach loops now output data frames without need to convert manually. Also made compatible with changes made in simulation_functions.R

--- simulations.R
+++ simulations.R
@@ -13,7 +13,7 @@ pars <- list(lambda0 = 1, sigma = 1)
 mask <- make.mask(traps = transect_list, buffer = 0, spacing = 10)
 times <- c(36500, 36530, 36560)
 
-cl <- makeCluster(20)
+cl <- makeCluster(2)
 registerDoParallel(cl)
 getDoParWorkers()
 
@@ -21,29 +21,25 @@ getDoParWorkers()
 set.seed(012345)
 Sys.time()
 
-N_expected <- 1:200
+N_expected <- 1:5
 B <- length(N_expected)
 
-output <- foreach(i = 1:B, .packages = c("tidyverse", "secr")) %dopar% {
+output <- foreach(i = 1:B, .packages = c("tidyverse", "secr"), .combine = rbind) %dopar% {
   N_exp <- N_expected[i]
   pop <- prune(population(N_exp, 365 * 101), 36500 - 30)
   
-  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-  mod <- secr::secr.fit(capthist = capthist$secr_capthist, mask = mask)
+  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, t0=36500-30)
+  mod <- secr::secr.fit(capthist = capthist$capthist, mask = mask)
   
-  list(expected = N_exp,
-       present = pres_det(pop, 36500)$pres,
-       detectable = pres_det(pop, 36500)$det,
-       estimated = region.N(mod)[2, 1]
+  data.frame(expected = N_exp,
+             present = pres_det(pop, 36500)$pres,
+             detectable = pres_det(pop, 36500)$det,
+             estimated = region.N(mod)[2, 1]
   )
 }
 
-df <- data.frame(expected = sapply(output, function(item) item$expected),
-                 present = sapply(output, function(item) item$present),
-                 detectable = sapply(output, function(item) item$detectable),
-                 estimated = sapply(output, function(item) item$estimated))
-
-saveRDS(df, "exp_pres_det_est.rds")
+beep()
+saveRDS(output, "exp_pres_det_est.rds")
 
 
 
@@ -51,7 +47,7 @@ saveRDS(df, "exp_pres_det_est.rds")
 mean_lifes <- 7:372
 B <- length(mean_lifes)
 
-output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
+output <- foreach(i = 1:B, .packages = c("tidyverse", "secr"), .combine = rbind) %dopar% {
   mean_life <- mean_lifes[i]
   pop <- population(30, 365 * 101, mean_life = mean_life)
   
@@ -63,28 +59,20 @@ output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
   
   slope <- mean_detectable_time / (365 + 2)
   
-  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-  mod <- secr::secr.fit(capthist = capthist$secr_capthist, mask = mask)
+  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, t0=36500-30)
+  mod <- secr::secr.fit(capthist = capthist$capthist, mask = mask)
   
-  list(estimated = region.N(mod)[2, 1],
-       present = pres_det(pop, 36500)$pres,
-       detectable = pres_det(pop, 36500)$det,
-       mean_life = mean_life,
-       mean_detectable_time = mean_detectable_time,
-       se = se,
-       slope = slope
+  data.frame(estimated = region.N(mod)[2, 1],
+             present = pres_det(pop, 36500)$pres,
+             detectable = pres_det(pop, 36500)$det,
+             mean_life = mean_life,
+             mean_detectable_time = mean_detectable_time,
+             se = se,
+             slope = slope
   )
 }
 
-df <- data.frame(mean_life = sapply(output, function(item) item$mean_life),
-                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
-                 se = sapply(output, function(item) item$se),
-                 slope = sapply(output, function(item) item$slope),
-                 present = sapply(output, function(item) item$present),
-                 estimated = sapply(output, function(item) item$estimated),
-                 detectable = sapply(output, function(item) item$detectable))
-
-saveRDS(df, "dropping_life.rds")
+saveRDS(output, "dropping_life.rds")
 
 
 
@@ -92,7 +80,7 @@ saveRDS(df, "dropping_life.rds")
 dropping_rate <- seq(1/7, 2, length.out = 200)
 B <- length(dropping_rate)
 
-output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
+output <- foreach(i = 1:B, .packages = c("tidyverse", "secr"), .combine = rbind) %dopar% {
   drop_rate <- dropping_rate[i]
   pop <- population(30, 365 * 101, drop_rate = drop_rate)
   
@@ -104,28 +92,20 @@ output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
   
   slope <- mean_detectable_time / (365 + (1 / drop_rate))
   
-  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-  mod <- secr::secr.fit(capthist = capthist$secr_capthist, mask = mask)
+  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, t0=36500-30)
+  mod <- secr::secr.fit(capthist = capthist$capthist, mask = mask)
   
-  list(drop_rate = drop_rate,
-       mean_detectable_time = mean_detectable_time,
-       se = se,
-       slope = slope,
-       estimated = region.N(mod)[2, 1],
-       present = pres_det(pop, 36500)$pres,
-       detectable = pres_det(pop, 36500)$det
+  data.frame(drop_rate = drop_rate,
+             mean_detectable_time = mean_detectable_time,
+             se = se,
+             slope = slope,
+             estimated = region.N(mod)[2, 1],
+             present = pres_det(pop, 36500)$pres,
+             detectable = pres_det(pop, 36500)$det
   )
 }
 
-df <- data.frame(drop_rate = sapply(output, function(item) item$drop_rate),
-                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
-                 se = sapply(output, function(item) item$se),
-                 slope = sapply(output, function(item) item$slope),
-                 estimated = sapply(output, function(item) item$estimated),
-                 present = sapply(output, function(item) item$present),
-                 detectable = sapply(output, function(item) item$detectable))
-
-saveRDS(df, "drop_rate.rds")
+saveRDS(output, "drop_rate.rds")
 
 
 
@@ -133,7 +113,7 @@ saveRDS(df, "drop_rate.rds")
 mean_stays <- seq(365/2, 365 * 1.5, length.out = 365)
 B <- length(mean_stays)
 
-output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
+output <- foreach(i = 1:B, .packages = c("tidyverse", "secr"), .combine = rbind) %dopar% {
   mean_stay <- mean_stays[i]
   pop <- population(30, 365 * 101, mean_stay = mean_stay)
   
@@ -145,27 +125,19 @@ output <- foreach(i = 1:B,  .packages = c("tidyverse", "secr")) %dopar% {
   
   slope <- mean_detectable_time / (mean_stay + 2)
   
-  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, HHN, pars, t0=36500-30)
-  mod <- secr::secr.fit(capthist = capthist$secr_capthist, mask = mask)
+  capthist <- survey_sim(x0, y0, x1, y1, pop, times = times, t0=36500-30)
+  mod <- secr::secr.fit(capthist = capthist$capthist, mask = mask)
   
-  list(mean_stay = mean_stay,
-       mean_detectable_time = mean_detectable_time,
-       se = se,
-       slope = slope,
-       estimated = region.N(mod)[2, 1],
-       present = pres_det(pop, 36500)$pres,
-       detectable = pres_det(pop, 36500)$det
+  data.frame(mean_stay = mean_stay,
+             mean_detectable_time = mean_detectable_time,
+             se = se,
+             slope = slope,
+             estimated = region.N(mod)[2, 1],
+             present = pres_det(pop, 36500)$pres,
+             detectable = pres_det(pop, 36500)$det
   )
 }
 
-df <- data.frame(mean_stay = sapply(output, function(item) item$mean_stay),
-                 mean_detectable_time = sapply(output, function(item) item$mean_detectable_time),
-                 se = sapply(output, function(item) item$se),
-                 slope = sapply(output, function(item) item$slope),
-                 estimated = sapply(output, function(item) item$estimated),
-                 present = sapply(output, function(item) item$present),
-                 detectable = sapply(output, function(item) item$detectable))
-
-saveRDS(df, "mean_stay.rds")
+saveRDS(output, "mean_stay.rds")
 stopCluster(cl)
 Sys.time()
